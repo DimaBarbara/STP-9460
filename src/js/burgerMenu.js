b3bb70962833ef6ad8f4353a6b1a4010f29a9a86
Dima <oxxTerm0s@gmail.com>
2025-07-20 11:48:47 +0300

    add basic section structure and implement desktop header

--- src/js/burgerMenu.js
+++ src/js/burgerMenu.js
@@ -1,8 +1,13 @@
 const burger = document.querySelector('#burger');
 const menu = document.querySelector('#menu');
 const background = document.querySelector('#primary-nav');
+const icon = document.querySelector('#burger-icon');
 
 burger.addEventListener('click', () => {
-  menu.classList.toggle('menu__list--active');
+  const isOpen = menu.classList.toggle('menu__list--active');
   background.classList.toggle('nav--active');
+  icon.setAttribute(
+    'href',
+    isOpen ? '../img/svg/close.svg' : '../img/svg/burger.svg'
+  );
 });
