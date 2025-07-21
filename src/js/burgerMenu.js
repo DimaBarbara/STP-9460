b8bcb02e07243e7dc3f207cc07aaa1e9bd13ce67
Dima <oxxTerm0s@gmail.com>
2025-07-22 02:56:50 +0300

    fix img

--- src/js/burgerMenu.js
+++ src/js/burgerMenu.js
@@ -10,7 +10,7 @@ burger.addEventListener('click', () => {
   background.classList.toggle('nav--active');
   icon.setAttribute(
     'href',
-    isOpen ? '../img/svg/close.svg' : '../img/svg/burger.svg'
+    isOpen ? '/img/svg/close.svg' : '/img/svg/burger.svg'
   );
 });
 menuLinks.forEach(link => {
@@ -18,7 +18,7 @@ menuLinks.forEach(link => {
     e.preventDefault();
     menu.classList.remove('menu__list--active');
     background.classList.remove('nav--active');
-    icon.setAttribute('href', '../img/svg/burger.svg');
+    icon.setAttribute('href', '/img/svg/burger.svg');
 
     const href = link.getAttribute('href');
     const targetSection = document.querySelector(href);
